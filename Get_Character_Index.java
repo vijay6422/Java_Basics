49bce83453beecfbe6ac136d3707765dd7c833a9
vijay6422 <vijaykumarreddy9121@gmail.com>
2024-06-16 17:36:12 -0400

    Character

---
+++ Get_Character_Index.java
@@ -0,0 +1,28 @@
+package JavaPrograms;
+
+public class Get_Character_Index {
+
+
+    public static void main(String[] args) {
+
+        String str = "Province soft";
+        System.out.println("Original string " + str);
+
+        //Returning the Character
+
+        char c = str.charAt(0);
+        char c1 = str.charAt(5);
+
+        System.out.println(c);
+        System.out.println(c1);
+    }
+
+        // Returning the Character Index
+
+        /*int s1 = str.charAt(0);
+        int s2 = str.charAt(5);
+
+        System.out.println((char) s1);
+        System.out.println((char) s2);
+    }*/
+}
